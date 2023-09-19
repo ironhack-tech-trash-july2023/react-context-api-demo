8e96e760625bb0a10891d4fe44c292378f3ba68d
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-09-19 14:23:40 +0200

    (context api) functionality to switch theme

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/theme.context";
+
 function ProjectCard(props) {
+
+  const {theme} = useContext(ThemeContext);
+
     return (
-      <div className="ProjectCard">
+      <div className={"ProjectCard " + theme}>
         <h3>{props.project.name}</h3>
         <p>{props.project.technologies}</p>
       </div>
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import ProjectCard from "../components/ProjectCard";
 import projectsData from "../data/projects-data.json";
+import { ThemeContext } from "../context/theme.context";
 
 function ProjectsPage() {
   const [projects, setProjects] = useState(projectsData);
 
+  const {theme} = useContext(ThemeContext);
+
   return (
-    <div className="ProjectsPage">
+    <div className={"ProjectsPage " + theme}>
       <h1>My Projects</h1>
       <div className="projects">
         {projects.map((p) => (
